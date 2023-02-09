98fe4888613ef92d0c11eb40bebebcb8d296b980
Ruhbsn <ruhban13@hotmail.com>
2023-02-09 16:18:30 +0400

    test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int x =  1;
+        System.out.println("Commit and push");
         System.out.println(x);
     }
 }
